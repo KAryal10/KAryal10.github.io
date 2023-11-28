a1fa1dc49d12021d15b6fdfb2086fb4c51d57a53
Kritan Aryal <kritanaryal@Kritans-Air.hitronhub.home>
2023-11-28 17:52:36 -0500

    added project picture

--- src/components/resume/resume.jsx
+++ src/components/resume/resume.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './resume-style.css'
 import Pdf from './Kritan_Aryal_Resume.pdf'
+import ballRollImg from './ball roll.png'
+import website from './website.png'
+import geometric from './geometric.png'
 
 export default function Resume() {
     return ( 
@@ -29,21 +32,21 @@ export default function Resume() {
 
                 <div class="project-wrapper">
                   <section class="project-item">
-                    <img src="images/bg-home-office-bookshelf.jpg" srcset="images/bg-home-office-bookshelf.jpg 2x" alt="Decoded by Christina YouTube video thumbnails" />
+                    <img src={ballRollImg}  alt="ball roll game" />
                     <h3>Roll the Ball Game</h3>
                     <p>In the creation of my ball-rolling game using Blueprint and C++ in Unreal Engine 5, I seamlessly blended programming prowess with a keen focus on user engagement. The game not only showcases technical proficiency but also emphasizes the importance of precision and strategy. By meticulously designing a set of instructions for players to follow, I transformed the gaming experience into a challenging yet rewarding journey. The use of Unreal Engine 5 allowed for stunning visuals and smooth gameplay mechanics, enhancing the overall immersion. Through thoughtful level design and intricate coding, players must navigate the virtual environment with precision, mastering the art of control to achieve victory. This project not only highlights my expertise in game development but also underscores my commitment to crafting an engaging user experience that captivates players and fosters a sense of accomplishment upon successful completion of the game's challenges.</p>
                     {/* <a class="btn" href="https://www.youtube.com/@madebychristina" target="_blank">See the game</a> */}
                   </section>
 
                   <section class="project-item">
-                    <img src="images/bg-home-office-bookshelf.jpg" srcset="images/bg-home-office-bookshelf.jpg 2x" alt="List of course names and thumbnails" />
+                    <img src={website}  alt="Screenshot of my website" />
                     <h3>Personal Website</h3>
                     <p>Creating my personal website using React was a testament to my skills in web design, front-end development, and content presentation. The website serves as a dynamic platform that not only showcases my professional background but also presents my projects and resume in a visually appealing and user-friendly manner. Leveraging the power of React, I implemented responsive design principles, ensuring that the website seamlessly adapts to various devices and screen sizes. The incorporation of a clean and intuitive user interface enhances the overall user experience, allowing visitors to navigate effortlessly through the comprehensive information about my skills, experiences, and accomplishments. The use of React's component-based architecture not only streamlined the development process but also facilitated easy maintenance and scalability. Through this project, I not only demonstrated my technical proficiency but also my commitment to creating a visually compelling and informative online presence that effectively communicates my capabilities and achievements.</p>
                     {/* <a class="btn" href="index.html" target="_blank">Go to website</a> */}
                   </section>
 
                   <section class="project-item">
-                    <img src="images/bg-home-office-bookshelf.jpg" srcset="images/bg-home-office-bookshelf.jpg 2x" alt="The TROY podcast website homepage" />
+                    <img src={geometric} alt="screenshot of geometric analysis project" />
                     <h3>Geometric Analysis</h3>
                     <p>In the development of my Java program, I undertook the task of reading and processing files containing coordinates of triangles, rectangles, and points, showcasing my adeptness in both programming and data analysis. Employing advanced algorithms, the program systematically determined relationships between the geometric shapes, meticulously checking for containment and overlap. The results were elegantly presented in a tabular format, reflecting my commitment to clear and concise data representation. Notably, the program calculated and displayed the percentage of points residing within the geometric figures, providing a quantitative measure of spatial relationships. This project not only demonstrated my proficiency in Java programming but also highlighted my analytical skills in interpreting and presenting meaningful insights from complex datasets. The meticulous attention to detail in handling geometric relationships and the effective communication of results underscored my ability to merge technical expertise with a comprehensive understanding of data analysis.</p>
                     {/* <a class="btn" href="https://troypodcast.com/" target="_blank">Check this project</a> */}
