c693814237d082492edbb4c55b83e7ec1a368ce8
Kritan Aryal <kritanaryal@Kritans-Air.hitronhub.home>
2023-11-28 14:37:33 -0500

    changing resume link

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
                     <main>
                         <p>Kritan Aryal is a driven and dedicated student with a passion for computer science and engineering. His academic achievements and hands-on experience demonstrate his commitment to excellence, problem-solving abilities, and a wide range of technical skills. Kritan's personal projects and leadership roles exemplify his enthusiasm for continuous growth and community engagement. As a Russell J. Ebeid Scholar, he is poised to make a significant impact in the field of technology and engineering. Connect with Kritan on <a href="https://www.linkedin.com/in/kritan-aryal-45ab55234/" target="_blank">LinkedIn</a> to stay updated on his exciting journey!
                         </p> 
-                        <p>To learn more about me, check out my <a href="resume.html"> resume</a>. Thanks for stopping by!</p>
+                        <p>To learn more about me, check out my <Link to ="/resume" >RESUME</Link>. Thanks for stopping by!</p>
                     </main>
                     </div>
                 </div>
