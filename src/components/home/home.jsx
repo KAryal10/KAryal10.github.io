d4722293f5b97a12c40f34d875a5844a77f36b17
Kritan Aryal <kritanaryal@Kritans-Air.hitronhub.home>
2023-12-02 12:55:43 -0500

    add headshot and fonts

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Home-styles.css';
 import { Link } from 'react-router-dom';
-
+import myPic from './IMG_6218.jpeg'
 
 export default function Home() {
     return (    
@@ -9,14 +9,16 @@ export default function Home() {
             <div  class="content-wrapper"> 
                 <div>
                 <div class="content-bg">
+                    <img src={myPic} alt="My headshot" />
                     <div class="content-inside">
                     <header>
                         <h1>Kritan Aryal</h1>
-                        <h3>B.S. Computer Science</h3>
+                       
                     </header>
 
                 
                     <main class="home-content" >
+                        <h3>About</h3>
                         <p>Kritan Aryal is a driven and dedicated student with a passion for computer science and engineering. His academic achievements and hands-on experience demonstrate his commitment to excellence, problem-solving abilities, and a wide range of technical skills. Kritan's personal projects and leadership roles exemplify his enthusiasm for continuous growth and community engagement. As a Russell J. Ebeid Scholar, he is poised to make a significant impact in the field of technology and engineering. Connect with Kritan on <a href="https://www.linkedin.com/in/kritan-aryal-45ab55234/" target="_blank">LinkedIn</a> to stay updated on his exciting journey!
                         </p> 
                         <p>To learn more about me, check out my <Link to ="/resume" >resume</Link>. Thanks for stopping by!</p>
